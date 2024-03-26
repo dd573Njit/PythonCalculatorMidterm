e841e67eea1fce25ab8c448807d3e10f6b08d896
Dhananjay Dubey <dubeydhananjay9@gmail.com>
2024-03-26 19:01:07 -0400

    deleted save command

--- app/plugins/history/save/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.command.base_command import BaseCommand
-
-class SaveCommand(BaseCommand):
-    def execute(self, *args):
-        if len(args) > 0:
-            print("The save command does not accept any arguments.")
-        else:
-            self.history_instance.save_history()
-            print("Calculation history saved.")
