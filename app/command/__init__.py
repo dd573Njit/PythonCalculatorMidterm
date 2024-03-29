2a432889d77af68ec03a3e569e55c248f060adba
Dhananjay Dubey <dubeydhananjay9@gmail.com>
2024-03-29 18:01:28 -0400

    Added project doc and video

--- app/command/__init__.py
+++ app/command/__init__.py
@@ -19,9 +19,7 @@ class CommandHandler:
         try:
             # Execute the command with any arguments provided
             command = self.commands[command_name]
-            print(command_name)
             result = command.execute(*args)  # Unpack arguments to pass to execute
-            print(result)
             if result is not None:
                 LoggingUtility.info(result)  # Or handle the command result as needed
         except KeyError:
